466bc3f5b87680a66b6bcafd9205c02be7679f16
codesinging <codesinging@gmail.com>
2022-06-23 16:47:11 +0800

    更新登录页面

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,70 @@
 <template>
-    <div>login</div>
+    <div class="flex items-center justify-center h-screen">
+        <el-card class="w-96" header="管理员登录">
+            <el-form :model="data" ref="form" :rules="rules" @keyup.enter="submit" :disabled="statuses.submit||statuses.redirect">
+                <el-form-item prop="username">
+                    <el-input v-model="data.username" placeholder="请输入登录账号">
+                        <template #prepend>
+                            <div class="w-12">登录账号</div>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input v-model="data.password" placeholder="请输入登录密码" show-password>
+                        <template #prepend>
+                            <div class="w-12">登录密码</div>
+                        </template>
+                    </el-input>
+                </el-form-item>
+
+                <div class="mt-2">
+                    <el-button type="primary" @click="submit" class="w-full" :loading="statuses.submit||statuses.redirect">登录</el-button>
+                </div>
+            </el-form>
+        </el-card>
+    </div>
 </template>
 
 <script setup>
-import {success} from "../utils/message";
+import {reactive, ref} from "vue";
+import api from "../utils/api";
+import {warning} from "../utils/message";
+import useStatus from "../states/status";
+import auth from "../utils/auth";
+import {useRouter} from "vue-router";
+import {appConfig} from "../config";
 
-import {alert, confirm} from "../utils/messageBox";
+const router = useRouter()
+const status = useStatus()
+const statuses = status.status
 
-confirm('delete ? ')
+const data = reactive({
+    username: import.meta.env.VITE_ADMIN_USERNAME,
+    password: import.meta.env.VITE_ADMIN_PASSWORD,
+})
+
+const rules = {
+    username: {required: true, message: '请输入登录账号'},
+    password: {required: true, message: '请输入登录密码'}
+}
+
+const form = ref()
+
+const submit = () => {
+    form.value.validate(valid => {
+        if (valid) {
+            api('auth/login').label('submit').catch().put(data).then(res => {
+                auth.login(res.token, res.admin)
+
+                status.set('redirect')
+
+                router.push(appConfig.home).then(() => status.unset('redirect'))
+            })
+        } else {
+            warning('表单验证未通过，请重新填写')
+        }
+    })
+}
 </script>
 
 <style scoped>
