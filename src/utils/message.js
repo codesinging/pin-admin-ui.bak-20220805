94e0bbaaf8dd47e3d15cf4ad3e3b75c897d1ec38
codesinging <codesinging@gmail.com>
2022-06-23 09:41:22 +0800

    增加消息提示工具

---
+++ src/utils/message.js
@@ -0,0 +1,28 @@
+import {ElMessage} from "element-plus";
+import 'element-plus/es/components/message/style/css'
+
+export default ElMessage
+
+export const success = (message) => {
+    ElMessage({
+        type: 'success',
+        message,
+        showClose: true,
+    })
+}
+
+export const warning = (message) => {
+    ElMessage({
+        type: 'warning',
+        message,
+        showClose: true,
+    })
+}
+
+export const error = (message) => {
+    ElMessage({
+        type: 'error',
+        message,
+        showClose: true,
+    })
+}
