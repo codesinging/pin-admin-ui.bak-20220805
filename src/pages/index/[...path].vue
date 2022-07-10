20d2e541d350565e93882f8b4ba860403bc93654
codesinging <codesinging@gmail.com>
2022-07-10 18:40:59 +0800

    优化 404 页面的显示内容

--- src/pages/index/[...path].vue
+++ src/pages/index/[...path].vue
@@ -1,10 +1,20 @@
 <template>
     <div class="h-full mt-40 flex items-center justify-center">
-        <el-result icon="error" title="404 页面不存在" sub-title="页面已删除或者不存在"></el-result>
+        <el-result icon="error" title="404 页面不存在" :sub-title="subTitle">
+            <template #extra>
+                <el-button type="primary" @click="layout.closePage()">关闭</el-button>
+            </template>
+        </el-result>
     </div>
 </template>
 
 <script setup>
+import {computed} from "vue";
+import useLayout from "../../states/layout.js";
+
+const layout = useLayout()
+
+const subTitle = computed(() => `当前页面(${layout.activeTab.path})不存在`)
 </script>
 
 <style scoped>
