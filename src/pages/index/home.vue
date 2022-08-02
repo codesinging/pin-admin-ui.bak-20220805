d3401c64135fc442454624df771dc21b8decaead
codesinging <codesinging@gmail.com>
2022-08-02 16:44:41 +0800

    修改 home 页面，增加上次登录信息

--- src/pages/index/home.vue
+++ src/pages/index/home.vue
@@ -6,12 +6,18 @@
                     <el-avatar size="large">{{ user.name }}</el-avatar>
                 </div>
                 <div class="space-y-1">
-                    <div class="text-lg">{{ user.name }}</div>
-                    <div class="space-x-1">
-                        <el-tag v-if="user.super" type="danger" size="small">超级管理员</el-tag>
-                        <template v-else>
-                            <el-tag v-for="role in user.roles" :key="role.id" size="small">{{ role.name }}</el-tag>
-                        </template>
+                    <div class="flex items-center space-x-2">
+                        <div class="text-lg">{{ user.name }}</div>
+                        <div class="space-x-1 flex items-center">
+                            <el-tag v-if="user.super" type="danger" size="small">超级管理员</el-tag>
+                            <template v-else>
+                                <el-tag v-for="role in user.roles" :key="role.id" size="small">{{ role.name }}</el-tag>
+                            </template>
+                        </div>
+                    </div>
+                    <div class="text-sm text-gray-400 divide-x">
+                        <span class="pr-2">上次登录时间：{{ lastLogin.time }}</span>
+                        <span class="px-2">上次登录IP：{{ lastLogin.ip }}</span>
                     </div>
                 </div>
             </div>
@@ -23,17 +29,26 @@
 <script setup>
 import useAuth from "../../states/auth.js";
 import {useTimestamp} from "@vueuse/core";
-import {computed} from "vue";
+import {computed, ref} from "vue";
 import dayjs from "dayjs";
 import 'dayjs/locale/zh-cn'
+import api from "../../utils/api";
 
 const auth = useAuth()
 const user = computed(() => auth.user)
 
+const lastLogin = ref({})
+
+const refreshLastLogin = () => {
+    api('auth/last_login').label('refreshLastLogin').get().then(res => lastLogin.value = res)
+}
+
+refreshLastLogin()
+
 dayjs.locale('zh-cn')
 
 const {timestamp} = useTimestamp({controls: true})
-const time = computed(() => dayjs(timestamp.value).format('M月D日 dddd HH:mm:ss'))
+const time = computed(() => dayjs(timestamp.value).format('YYYY年MM月DD日 dddd HH:mm:ss'))
 </script>
 
 <style scoped>
